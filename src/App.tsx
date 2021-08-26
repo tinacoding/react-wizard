80f4d14960de17866c2b36a294a020aed2ecfe87
Tina G <tina@smartlogic.io>
2021-08-25 21:05:15 -0400

    adds chakra ui for brevity

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Center, Flex, Heading } from "@chakra-ui/react";
+
 function App(): JSX.Element {
-  return <div>hello world</div>;
+  return (
+    <Center bg="tomato" h="100px" color="white">
+      <Flex direction="column" justifyContent="center" alignItems="center">
+        <Heading as="h1" size="4xl" isTruncated>
+          This is the Center
+        </Heading>
+        <p>hello world</p>
+      </Flex>
+    </Center>
+  );
 }
 
 export default App;
