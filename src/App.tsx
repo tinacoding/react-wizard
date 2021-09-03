db6e0c6ed11cbcd969ef1ea4e3eec1d9e629af98
Tina G <tina@smartlogic.io>
2021-09-03 16:11:08 -0400

    Creates barebones wizard
    :: Components ::
    
    <<Step1>>
    A simple functional component that returns a p tag with static text
    
    <<Step2>>
    A simple functional component that returns a p tag with static text
    
    <<App>>
    Adds state for the currentStep and progress of the wizard. Creates two
    function for traversing the wizard, previous and next.
    - previous: takes no params and decrements the currentStep
    - next: takes no params and increments the currentStep
    
    -Adds a useEffect that depends on the currentStep and steps array to
    calculate the progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,61 @@
-import { Center, Flex, Heading } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+function Step1(): JSX.Element {
+  return (
+    <p>Step 1</p>
+  );
+};
+
+function Step2(): JSX.Element {
+  return (
+    <p>Step 2</p>
+  );
+};
 
 function App(): JSX.Element {
+  const [currentStep, setCurrentStep] = useState(0);
+  const steps = [Step1, Step2];
+  const Content = steps[currentStep];
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (steps.length - 1 > 0) {
+      const percentage = parseInt((currentStep / (steps.length - 1)).toPrecision(), 10) * 100;
+      setProgress(percentage);
+    }
+  }, [currentStep, steps])
+
+  const previous = () => {
+    if (currentStep > 0) {
+      setCurrentStep((prev) => prev - 1);
+    }
+  };
+
+  const next = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
+
   return (
-    <Center bg="tomato" h="100px" color="white">
-      <Flex direction="column" justifyContent="center" alignItems="center">
-        <Heading as="h1" size="4xl" isTruncated>
-          This is the Center
-        </Heading>
-        <p>hello world</p>
+    <Flex direction="column">
+      <section>
+        <Content />
+        Progress: {progress}%
+      </section>
+      <Flex direction="column" alignItems="center" justifyContent="center">
+        {currentStep}
+        <div>
+          <button type="button" onClick={previous}>
+            Back
+          </button>
+          <button type="button" onClick={next}>
+            Next
+          </button>
+        </div>
       </Flex>
-    </Center>
+    </Flex>
   );
 }
 
