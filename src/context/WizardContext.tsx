4e482c4535b82d250e4b77c7f22c17861232d949
Tina G <tina@smartlogic.io>
2021-09-06 14:20:36 -0400

    React useCallback

--- src/context/WizardContext.tsx
+++ src/context/WizardContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 
 type SideEffect = () => void;
 
@@ -11,22 +11,26 @@ const initialState = {
 
 export const WizardContext = createContext(initialState);
 
-type WizardProviderProps = { children: React.ReactNode, steps: JSX.Element[]};
+type WizardProviderProps = { children: React.ReactNode; steps: JSX.Element[] };
 
 export function WizardProvider({ children, steps }: WizardProviderProps): JSX.Element {
   const [currentStep, setCurrentStep] = useState(0);
 
-  const previous = () => {
+  const previous = useCallback(() => {
     if (currentStep > 0) {
       setCurrentStep((prev) => prev - 1);
     }
-  };
+  }, [currentStep]);
 
-  const next = () => {
+  const next = useCallback(() => {
     if (currentStep < steps.length - 1) {
       setCurrentStep((prev) => prev + 1);
     }
-  };
+  }, [currentStep, steps]);
 
-  return <WizardContext.Provider value={{ currentStep, totalSteps: steps.length, previous, next }}>{children}</WizardContext.Provider>;
+  return (
+    <WizardContext.Provider value={{ currentStep, totalSteps: steps.length, previous, next }}>
+      {children}
+    </WizardContext.Provider>
+  );
 }
