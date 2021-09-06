b610acadd84ff1e7ea7e1600d9950bbcf1994809
Tina G <tina@smartlogic.io>
2021-09-06 14:20:02 -0400

    Wizard Context

---
+++ src/context/WizardContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+type SideEffect = () => void;
+
+const initialState = {
+  currentStep: 0,
+  totalSteps: 0,
+  previous: (() => null) as SideEffect,
+  next: (() => null) as SideEffect,
+};
+
+export const WizardContext = createContext(initialState);
+
+type WizardProviderProps = { children: React.ReactNode, steps: JSX.Element[]};
+
+export function WizardProvider({ children, steps }: WizardProviderProps): JSX.Element {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const previous = () => {
+    if (currentStep > 0) {
+      setCurrentStep((prev) => prev - 1);
+    }
+  };
+
+  const next = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
+
+  return <WizardContext.Provider value={{ currentStep, totalSteps: steps.length, previous, next }}>{children}</WizardContext.Provider>;
+}
