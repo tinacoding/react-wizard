69af2371e4c0db726dcc476e408331ad6fce34ab
Tina G <tina@smartlogic.io>
2021-09-08 13:14:01 -0400

    🧪 Bonus: Testing our Context

--- src/context/__tests__/WizardContext.test.tsx
+++ src/context/__tests__/WizardContext.test.tsx
@@ -4,60 +4,70 @@ import { render, screen } from "@testing-library/react";
 import WizardConsumer from "src/context/helpers/WizardConsumer";
 import { WizardContext } from "src/context/WizardContext";
 
-const Step1 = () => <p>Step 1</p>
-const Step2 = () => <p>Step 2</p>
+const Step1 = () => <p>Step 1</p>;
+const Step2 = () => <p>Step 2</p>;
 
 test("WizardConsumer shows default value", () => {
-	render(<WizardConsumer />);
+  render(<WizardConsumer />);
 
   expect(screen.getByText(/^Current step:/)).toHaveTextContent("0");
   expect(screen.getByText(/^Total steps:/)).toHaveTextContent("0");
 });
 
 type ProviderProps = {
-	value: {
-		currentStep: number;
-		steps: (() => JSX.Element)[];
-		totalSteps: number;
-		previous: () => void;
-		next: () => void;
-	}
+  value: {
+    currentStep: number;
+    steps: (() => JSX.Element)[];
+    totalSteps: number;
+    previous: () => void;
+    next: () => void;
+  };
 };
 
 type CustomRenderOptions = {
-	providerProps: ProviderProps,
-	[x: string]: unknown
-}
+  providerProps: ProviderProps;
+  [x: string]: unknown;
+};
 
-const customRender = (ui: JSX.Element, { providerProps, ...renderOptions }: CustomRenderOptions) => render(<WizardContext.Provider {...providerProps}>{ui}</WizardContext.Provider>, renderOptions);
+const customRender = (ui: JSX.Element, { providerProps, ...renderOptions }: CustomRenderOptions) =>
+  render(<WizardContext.Provider {...providerProps}>{ui}</WizardContext.Provider>, renderOptions);
 
 test("WizardConsumer shows value from provider", () => {
-	const steps = [Step1, Step2]
+  const steps = [Step1, Step2];
   const providerProps = {
     value: {
       currentStep: 1,
       steps,
       totalSteps: steps.length - 1,
-      previous: (() => null),
-      next: (() => null),
+      previous: () => null,
+      next: () => null,
     },
   };
   customRender(<WizardConsumer />, { providerProps });
-	expect(screen.getByText(/^Current step:/)).toHaveTextContent("1");
+  expect(screen.getByText(/^Current step:/)).toHaveTextContent("1");
   expect(screen.getByText(/^Total steps:/)).toHaveTextContent("1");
   expect(screen.getByText(/^Step 1/)).toBeInTheDocument();
   expect(screen.getByText(/^Step 2/)).toBeInTheDocument();
 });
 
 test("WizardProvider shows full name", () => {
-	const providerProps = {
-		value: {
-			first: "Jack",
-			last: "Dawson",
-		}
+  const providerProps = {
+    value: {
+      first: "Jack",
+      last: "Dawson",
+    },
   };
-	customRender(<WizardContext.Consumer>{({ first, last }) => <span>Received: {first} {last}</span>}</WizardContext.Consumer>, {
-    providerProps,
-  });
+  customRender(
+    <WizardContext.Consumer>
+      {({ first, last }) => (
+        <span>
+          Received: {first} {last}
+        </span>
+      )}
+    </WizardContext.Consumer>,
+    {
+      providerProps,
+    }
+  );
   expect(screen.getByText(/^Received:/).textContent).toBe("Received: Jack Dawson");
 });
--- src/context/helpers/WizardConsumer.tsx
+++ src/context/helpers/WizardConsumer.tsx
@@ -2,17 +2,21 @@ import { useContext } from "react";
 import { WizardContext } from "src/context/WizardContext";
 
 function WizardConsumer(): JSX.Element {
-	const { currentStep, steps, totalSteps, previous, next } = useContext(WizardContext);
-	return (
-		<>
-			<p>Current step: {currentStep}</p>
-			<p>Total steps: {totalSteps}</p>
-			{/* eslint-disable-next-line react/no-array-index-key */}
-			<div>{steps.length && steps.map((Step, idx) => <Step key={idx} />)}</div>
-			<button type="button" onClick={previous}>Back</button>
-			<button type="button" onClick={next}>Next</button>
-		</>
-	)
+  const { currentStep, steps, totalSteps, previous, next } = useContext(WizardContext);
+  return (
+    <>
+      <p>Current step: {currentStep}</p>
+      <p>Total steps: {totalSteps}</p>
+      {/* eslint-disable-next-line react/no-array-index-key */}
+      <div>{steps.length && steps.map((Step, idx) => <Step key={idx} />)}</div>
+      <button type="button" onClick={previous}>
+        Back
+      </button>
+      <button type="button" onClick={next}>
+        Next
+      </button>
+    </>
+  );
 }
 
 export default WizardConsumer;
